91f21573d68dcea9043ce283181f87132c77a290
Jerry Chen <zgchen559@gmail.com>
2019-04-06 22:07:06 -0700

    Netflix example with importing data

--- Day2PythonNotes.py
+++ Day2PythonNotes.py
@@ -1,4 +1,5 @@
 # Import File
+
 import random
 import string  # all imports at the top
 
@@ -12,16 +13,67 @@ for x in range(8):  # iterate from index 0 through 7
 
 # Read File
 
-file = 'Resources/input.txt'  # store file path associated with file
+# Old way to open CSV
+# file = 'Resources/input.txt'  # store file path associated with file
 
-with open(file, 'r') as text:  # r is read
-    print(text)
+# with open(file, 'r') as text:  # r is read
+#     print(text)
 
-    lines = text.read()  # store text in a variable called lines
-    print(lines)
+#     lines = text.read()  # store text in a variable called lines
+#     print(lines)
+
+# Correct way to open CSV
+# import csv
+# import os
+# csvpath = os.path.join("Resources", "netflix_ratings.csv")
+# with open(csvpath, newline="") as csvfile:
+#     csvreader = csv.reader(csvfile, delimiter=",")
+#     print(csvreader)
+#     for row in csvreader:
+#         print(row)
 
 # ------------------------------------------------------------------
 
+# Write File
+
+# import csv
+# import os
+
+# # Specify the file to write to
+# output_path = os.path.join('output', 'new.csv')
+
+# # Open the file using "write" mode. Specify the variable to hold the contents
+# with open(output_path, 'w', newline='') as csvfile:
+
+#     # Initialize csv.writer
+#     csvwriter = csv.writer(csvfile, delimiter=',')
+
+#     # Write the first row (column headers)
+#     csvwriter.writerow(['First Name', 'Last Name', 'SSN'])
+
+#     # Write the second row
+#     csvwriter.writerow(['Caleb', 'Frost', '505-80-2901'])
 
 # ------------------------------------------------------------------
+
+# Zipping lists
+
+# Three Lists
+indexes = [1, 2, 3, 4]
+employees = ["Michael", "Dwight", "Meredith", "Kelly"]
+department = ["Boss", "Sales", "Sales", "HR"]
+
+# Zip all 3 lists together into tuples(immutable)
+roster = zip(indexes, employees, department)
+
+# print contents of each row.
+for employee in roster:
+    print(employee)
+
+# (1, 'Michael', 'Boss')
+# (2, 'Dwight', 'Sales')
+# (3, 'Meredith', 'Sales')
+# (4, 'Kelly', 'HR')
+
+
 # ------------------------------------------------------------------
