86e1da9940a84262591a21d15e756b9f3901524f
Jerry Chen <zgchen559@gmail.com>
2019-04-07 14:05:09 -0700

    Day 3 Python Notes. Git review

---
+++ Day3PythonNotes.py
@@ -0,0 +1,11 @@
+# # git commands review
+# git clone < repo_name >  # branch off master
+# # code your changes
+# git status  # see which files have not yet been commited.
+# git add .  # add all files
+# git commit - m "message for you and other devs to read in the future"  # commit code
+# git - am "message"  # add all files and commits
+# git status  # check status again before pushing
+# git push origin master  # push code to master
+
+# ------------------------------------------------------------
